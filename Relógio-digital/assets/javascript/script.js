24e88f6041f239340276fbc2cceaddd13282ecdf
RodrigoVasc <rodrigovasco.dev@gmail.com>
2023-05-18 20:41:40 -0300

    funções para tocar o alarme

--- Relógio-digital/assets/javascript/script.js
+++ Relógio-digital/assets/javascript/script.js
@@ -3,17 +3,20 @@ const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
 const buttonHabilitar = document.getElementById('habilitar');
 const start_button = document.getElementById('start');
-const horas_input = document.getElementsByClassName('h');
-const minutes_input = document.getElementsByClassName('m');
+const editAlarme = document.getElementById('editarAlarme');
+const parar = document.getElementById('parar');
+const overlay = document.getElementById("overlay");
+const volumeSlider = document.getElementById("volume");
 const audio = new Audio();
-
-
+let horaAlarme = 0;
+let minutoAlarme = 0;
+audio.src = 'assets/Extreme Alarm.mp3';
 const relogio = setInterval(function time(){
+    
     let dateToday = new Date();
     let hr = dateToday.getHours();
     let min = dateToday.getMinutes();
     let sec = dateToday.getSeconds();
-
     if (hr < 10) hr = '0' + hr;
     if (min < 10) min = '0' + min;
     if (sec < 10) sec = '0' + sec;
@@ -21,34 +24,42 @@ const relogio = setInterval(function time(){
     horas.textContent= hr;
     minutos.textContent= min;
     segundos.textContent= sec;
-})
-
-
-start_button.addEventListener("click", () => {
-    const horaInputValue = horas_input[0].value;
-    const minutoInputValue = minutes_input[0].value;
-    if (hr === horaInputValue && min === minutoInputValue) {
-        audio.src = 'assets/Extreme Alarm.mp3';
+    
+    if(hr === horaAlarme && min === minutoAlarme){
+        horaAlarme = 0
+        minutoAlarme = 0
+        console.log('tocou!')
+        audio.currentTime = 0;
         audio.play();
+        fecharJanela();
     }
+    audio.volume = volumeSlider.value;
+    volumeSlider.addEventListener("input", function () {
+        volume = volumeSlider.value;
 
-})
+    })
+});
+
+function agendarAlarme () {
+    const horas_input = document.getElementById('hour_input').value;
+    const minutos_input = document.getElementById('minutes_input').value;
+    horaAlarme = parseInt(horas_input);
+    minutoAlarme = parseInt(minutos_input);
+    parar.style.display = "block";
+    fecharJanela();
+}
 
 function abrirJanela () {
-    const editAlarme = document.getElementById('editarAlarme');
-    const overlay = document.getElementById("overlay");
     editAlarme.style.display = "block";
     overlay.style.display = "block";
 }
 
 function fecharJanela() {
-    const editAlarme = document.getElementById('editarAlarme');
-    const overlay = document.getElementById("overlay");
     editAlarme.style.display = "none";
     overlay.style.display = "none";
 }
 
-
 function stopAlarme() {
-    audio.stop();
+    audio.pause();
+    parar.style.display = "none";
 }
